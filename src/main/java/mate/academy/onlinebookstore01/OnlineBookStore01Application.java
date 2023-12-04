f0d1388e8bad264983dae63da6ca952251fccfec
Dmytro Potapov <potapovds1993@gmail.com>
2023-12-04 15:54:51 +0200

    init commit

---
+++ src/main/java/mate/academy/onlinebookstore01/OnlineBookStore01Application.java
@@ -0,0 +1,13 @@
+package mate.academy.onlinebookstore01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStore01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookStore01Application.class, args);
+    }
+
+}
---
+++ src/test/java/mate/academy/onlinebookstore01/OnlineBookStore01ApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.onlinebookstore01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStore01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
