9e9c34a84adb32915541787f21fe7730f0490256
Dmytro Potapov <potapovds1993@gmail.com>
2023-12-05 14:31:06 +0200

    added tableName and CommandLineRunner bean

--- src/main/java/mate/academy/onlinebookstore01/OnlineBookStore01Application.java
+++ src/main/java/mate/academy/onlinebookstore01/OnlineBookStore01Application.java
@@ -1,12 +1,28 @@
 package mate.academy.onlinebookstore01;
 
+import mate.academy.onlinebookstore01.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineBookStore01Application {
+    @Autowired
+    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStore01Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+
+            }
+        };
     }
 }
