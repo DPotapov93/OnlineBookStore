57f4c45f2e0fb12972b5559214eaf95dce655350
Dmytro Potapov <potapovds1993@gmail.com>
2023-12-15 15:59:08 +0200

    iplemented CustomGlobalExceptionHandler class and added meaningful validation

--- src/main/java/mate/academy/onlinebookstore01/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/onlinebookstore01/dto/CreateBookRequestDto.java
@@ -1,14 +1,24 @@
 package mate.academy.onlinebookstore01.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @Min(1)
     private Long id;
+    @NotNull
     private String title;
+    @NotNull
     private String author;
+    @NotNull
+    @Size(min = 10, max = 13)
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
     private String description;
     private String coverImage;
