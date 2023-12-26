887af2d8a9bb0e268520c161861ddd9123c902a3
Dmytro Potapov <potapovds1993@gmail.com>
2023-12-26 15:39:10 +0200

    fixed Liquibase changesets, fixed AuthController Class and fixed Pattern in BookRequestDto class

--- src/main/java/mate/academy/onlinebookstore01/controller/AuthenticationController.java
+++ src/main/java/mate/academy/onlinebookstore01/controller/AuthenticationController.java
@@ -1,5 +1,6 @@
 package mate.academy.onlinebookstore01.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import mate.academy.onlinebookstore01.dto.user.UserRegistrationRequestDto;
 import mate.academy.onlinebookstore01.dto.user.UserResponseDto;
@@ -7,6 +8,7 @@ import mate.academy.onlinebookstore01.exception.RegistrationException;
 import mate.academy.onlinebookstore01.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +21,7 @@ public class AuthenticationController {
 
     @PostMapping("/registration")
     @ResponseStatus(HttpStatus.CREATED)
-    public UserResponseDto register(UserRegistrationRequestDto request)
+    public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto request)
             throws RegistrationException {
         return userService.register(request);
     }
