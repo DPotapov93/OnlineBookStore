e6da4e83f71792df8774e3c4399ba4e7356ca279
Dmytro Potapov <potapovds1993@gmail.com>
2023-12-29 14:01:35 +0200

    added isDeleted to the methods and sign a changesets

--- src/main/java/mate/academy/onlinebookstore01/repository/BookRepository.java
+++ src/main/java/mate/academy/onlinebookstore01/repository/BookRepository.java
@@ -2,6 +2,8 @@ package mate.academy.onlinebookstore01.repository;
 
 import mate.academy.onlinebookstore01.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
--- src/main/java/mate/academy/onlinebookstore01/repository/RoleRepository.java
+++ src/main/java/mate/academy/onlinebookstore01/repository/RoleRepository.java
@@ -2,6 +2,8 @@ package mate.academy.onlinebookstore01.repository;
 
 import mate.academy.onlinebookstore01.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
